fe7878a79798357fa97fba839360b69235acfa15
junipalii <sydneyoricho50@gmail.com>
2026-07-14 15:09:36 +0300

    i finally figured out how to make a 10x10 table , in the printf statement i added i*j and added a println statement after the inner loop so i can have multiple rows after the inner loop iterates 10 times

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -6,11 +6,16 @@ Print a formatted 10x10 multiplication table where every number is right-aligned
 ...
 You'll need a nested for loop for this one — which is a preview of what's coming in arrays. Focus on the %Xd width specifier to keep columns aligned.
 The second one is the more useful exercise for locking in width. Start with whichever.
-
+*/
 import java.util.Scanner;
 public class MultiplicationTable {
-    static void main(String[] args){
-        for(int i=0;)
+public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=10;j++){
+                System.out.printf("%4d",i*j);
+            }
+            System.out.println();
+        }
     }
 }
-*/
+
