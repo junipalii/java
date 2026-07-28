04546cad66bd263c4accef795b3b5365c770f488
junipalii <sydneyoricho50@gmail.com>
2026-07-28 17:46:24 +0300

    fixed the bug on highest and lowest temp , by just comparing temps[i] to my tracker variables highest and lowest temp instead of temps[0] and [1] which i had assigned them to .

--- src/ArrayPractice2.java
+++ src/ArrayPractice2.java
@@ -4,17 +4,15 @@ Declare an array of 7 temperatures. Print the highest and lowest.
 public class ArrayPractice2 {
     public static void main(String[] args){
     //array declaration and initialization , variable declaration
-    int highestTemp=0;
-    int lowestTemp=0;
-    int temps[]={43,57,40,90,70,60,80};
+    int temps[]={44,57,40,90,70,43,80};
+    int lowestTemp=temps[0];
+    int highestTemp=temps[1];
     //finding he highest and lowest temperature
     for(int i = 0;i<temps.length;i++){
-        lowestTemp=temps[0];
-        highestTemp=temps[1];
-        if(temps[i]>temps[0]){
-            highestTemp=temps[0];
-        } else if (temps[i]<temps[0]) {
-            lowestTemp=temps[0];
+        if (temps[i]<lowestTemp) {
+            lowestTemp=temps[i];
+        } else if (temps[i]>highestTemp) {
+            highestTemp=temps[i];
         }
     }
         System.out.println("Highest temp :"+highestTemp);
